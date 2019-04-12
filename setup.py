7c891e86c23d79d111bb35401fa565b4d5759e55
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2019-04-12 16:30:31 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tensorflow-models',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/MihaiTheCoder/tensorflow-models',
+    license='',
+    author='',
+    author_email='',
+    description='')    
